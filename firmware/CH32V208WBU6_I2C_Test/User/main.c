f827292c755f550a5cde1e5bfa9a28ff514383c9
Deimos <deimoshall@gmail.com>
2023-09-18 12:37:52 -0600

    refactor: use i2c2 instead of i2c1

--- firmware/CH32V208WBU6_I2C_Test/User/main.c
+++ firmware/CH32V208WBU6_I2C_Test/User/main.c
@@ -13,7 +13,7 @@
 /*
  *@Note
  *7-bit addressing mode, master/slave mode, transceiver routine:
- *I2C1_SCL(PB8)\I2C1_SDA(PB9).
+ *I2C2_SCL(PB10)\I2C2_SDA(PB11).
  *This routine demonstrates that Master sends and Slave receives.
  *Note: The two boards download the Master and Slave programs respectively,
  *and power on at the same time.
@@ -25,22 +25,12 @@
 #include "debug.h"
 
 /* I2C Mode Definition */
-#define HOST_MODE   0
-#define SLAVE_MODE   1
 #define I2C_TIMEOUT  1000
 
-/* I2C Communication Mode Selection */
-#define I2C_MODE   HOST_MODE
-//#define I2C_MODE   SLAVE_MODE
-
 /* Global define */
-#define Size   6
-#define RXAdderss   0x02
-#define TxAdderss   0x3C
+#define TxAdderss   0x02
 
 /* Global Variable */
-u8 TxData[Size] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06 };
-u8 RxData[5][Size];
 
 /*********************************************************************
  * @fn      IIC_Init
@@ -55,15 +45,15 @@ void IIC_Init(u32 bound, u16 address)
     I2C_InitTypeDef I2C_InitTSturcture={0};
 
     RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOB | RCC_APB2Periph_AFIO, ENABLE );
-    GPIO_PinRemapConfig(GPIO_Remap_I2C1, ENABLE);
-    RCC_APB1PeriphClockCmd( RCC_APB1Periph_I2C1, ENABLE );
+    // GPIO_PinRemapConfig(GPIO_Remap_I2C1, ENABLE);
+    RCC_APB1PeriphClockCmd( RCC_APB1Periph_I2C2, ENABLE );
 
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_OD;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_Init( GPIOB, &GPIO_InitStructure );
 
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_OD;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_Init( GPIOB, &GPIO_InitStructure );
@@ -74,112 +64,40 @@ void IIC_Init(u32 bound, u16 address)
     I2C_InitTSturcture.I2C_OwnAddress1 = address;
     I2C_InitTSturcture.I2C_Ack = I2C_Ack_Enable;
     I2C_InitTSturcture.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
-    I2C_Init(I2C1, &I2C_InitTSturcture);
+    I2C_Init(I2C2, &I2C_InitTSturcture);
 
-    I2C_Cmd(I2C1, ENABLE);
-
-#if (I2C_MODE == HOST_MODE)
-    I2C_AcknowledgeConfig(I2C1, ENABLE);
-
-#endif
-}
-
-void I2C_MasterSlave_Test() {
-    u8 i = 0;
-    u8 j = 0;
-    u8 p = 0;
-
-#if (I2C_MODE == HOST_MODE)
-    printf("IIC Host mode\r\n");
-    IIC_Init( 80000, TxAdderss);
-
-    for( j =0; j < 5; j++)
-     {
-    while( I2C_GetFlagStatus( I2C1, I2C_FLAG_BUSY ) != RESET );
-
-    I2C_GenerateSTART( I2C1, ENABLE );
-
-    while( !I2C_CheckEvent( I2C1, I2C_EVENT_MASTER_MODE_SELECT ) );
-    I2C_Send7bitAddress( I2C1, 0x02, I2C_Direction_Transmitter );
-
-    while( !I2C_CheckEvent( I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED ) );
-
-    for( i=0; i< 6;i++ )
-    {
-        if( I2C_GetFlagStatus( I2C1, I2C_FLAG_TXE ) !=  RESET )
-        {
-            Delay_Ms(100);
-            I2C_SendData( I2C1, TxData[i] );
-        }
-    }
-
-    while( !I2C_CheckEvent( I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED ) );
-    I2C_GenerateSTOP( I2C1, ENABLE );
-    Delay_Ms(1000);
-     }
-
-#elif (I2C_MODE == SLAVE_MODE)
-    printf("IIC Slave mode\r\n");
-    IIC_Init( 80000, RXAdderss);
-
-    for( p =0; p < 5; p++)
-    {
-
-    i = 0;
-    while( !I2C_CheckEvent( I2C1, I2C_EVENT_SLAVE_RECEIVER_ADDRESS_MATCHED ) );
-    while( i < 6 )
-    {
-        if( I2C_GetFlagStatus( I2C1, I2C_FLAG_RXNE ) !=  RESET )
-        {
-            RxData[p][i] = I2C_ReceiveData( I2C1 );
-            i++;
-        }
-    }
-        I2C1->CTLR1 &= I2C1->CTLR1;
-    }
-        printf( "RxData:\r\n" );
-     for(p=0; p<5; p++)
-   {
-        for( i = 0; i < 6; i++ )
-        {
-           printf( "%02x ", RxData[p][i] );
-        }
-           printf( "\r\n ");
-     }
-
-
-#endif
+    I2C_Cmd(I2C2, ENABLE);
+    I2C_AcknowledgeConfig(I2C2, ENABLE);
 }
 
 void Scan_I2C_Devices() {
     printf("Scanning I2C bus...\n");
 
     for (uint8_t address = 1; address < 128; address++) {
-        uint8_t data = 0;
         uint32_t timeout = I2C_TIMEOUT;
-        while (I2C_GetFlagStatus(I2C1, I2C_FLAG_BUSY)) {
+        while (I2C_GetFlagStatus(I2C2, I2C_FLAG_BUSY)) {
             if (--timeout == 0) {
                 printf("I2C bus is busy\n");
-                return 1;
+                return;
             }
         }
-        I2C_GenerateSTART(I2C1, ENABLE);
+        I2C_GenerateSTART(I2C2, ENABLE);
         timeout = I2C_TIMEOUT;
-        while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT)) {
+        while (!I2C_CheckEvent(I2C2, I2C_EVENT_MASTER_MODE_SELECT)) {
             if (--timeout == 0) {
                 printf("Failed to generate start condition\n");
-                return 1;
+                return;
             }
         }
-        I2C_Send7bitAddress(I2C1, address << 1, I2C_Direction_Transmitter);
+        I2C_Send7bitAddress(I2C2, address << 1, I2C_Direction_Transmitter);
         timeout = I2C_TIMEOUT;
-        while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED)) {
+        while (!I2C_CheckEvent(I2C2, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED)) {
             if (--timeout == 0) {
-                printf("No response from address 0x%02X\n", address);
+                // printf("No response from address 0x%02X\n", address);
                 break;
             }
         }
-        I2C_GenerateSTOP(I2C1, ENABLE);
+        I2C_GenerateSTOP(I2C2, ENABLE);
         if (timeout > 0) {
             printf("Found device at address 0x%02X\n", address);
         }
@@ -198,9 +116,8 @@ int main(void)
     SystemCoreClockUpdate();
     Delay_Init();
     USART_Printf_Init(115200);
-    printf( "SystemClk:%d\r\n", SystemCoreClock );
-//    printf( "ChipID:%08x\r\n", DBGMCU_GetCHIPID() );
-    IIC_Init( 80000, TxAdderss);
+    // printf("SystemClk:%d\r\n", SystemCoreClock);
+    IIC_Init(80000, TxAdderss);
     Scan_I2C_Devices();
 
     while(1);
