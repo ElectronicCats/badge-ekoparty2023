95ed3c81b67784109949b5988376ef4f436e7a58
Deimos <deimoshall@gmail.com>
2023-09-19 10:27:12 -0600

    Add ssd1306 library

--- firmware/CH32V208WBU6_I2C_Test/User/main.c
+++ firmware/CH32V208WBU6_I2C_Test/User/main.c
@@ -23,6 +23,12 @@
  */
 
 #include "debug.h"
+//#include "i2c_oled.h"
+#include "ch32v003fun.h"
+#include <stdio.h>
+#include "ssd1306_i2c.h"
+#include "ssd1306.h"
+#include "bomb.h"
 
 /* I2C Mode Definition */
 #define I2C_TIMEOUT  1000
@@ -30,7 +36,32 @@
 /* Global define */
 #define TxAdderss   0x02
 
+/* Function protopotypes */
+void IIC_Init(u32 bound, u16 address);
+void Scan_I2C_Devices();
+void oled_test();
+
 /* Global Variable */
+
+/*********************************************************************
+ * @fn      main
+ *
+ * @brief   Main program.
+ *
+ * @return  none
+ */
+int main(void)
+{
+    SystemCoreClockUpdate();
+    Delay_Init();
+    USART_Printf_Init(115200);
+    // printf("SystemClk:%d\r\n", SystemCoreClock);
+    IIC_Init(80000, TxAdderss);
+    Scan_I2C_Devices();
+    oled_test();
+
+    while(1);
+}
 
 /*********************************************************************
  * @fn      IIC_Init
@@ -104,21 +135,117 @@ void Scan_I2C_Devices() {
     }
 }
 
-/*********************************************************************
- * @fn      main
- *
- * @brief   Main program.
- *
- * @return  none
- */
-int main(void)
+void oled_test()
 {
-    SystemCoreClockUpdate();
-    Delay_Init();
-    USART_Printf_Init(115200);
-    // printf("SystemClk:%d\r\n", SystemCoreClock);
-    IIC_Init(80000, TxAdderss);
-    Scan_I2C_Devices();
+    // 48MHz internal clock
+    SystemInit();
 
+    Delay_Ms( 100 );
+    printf("\r\r\n\ni2c_oled example\n\r");
+
+    // init i2c and oled
+    Delay_Ms( 100 );    // give OLED some more time
+    printf("initializing i2c oled...");
+    if(!ssd1306_i2c_init())
+    {
+        ssd1306_init();
+        printf("done.\n\r");
+
+        printf("Looping on test modes...");
+        while(1)
+        {
+            for(uint8_t mode=0;mode<(SSD1306_H>32?9:8);mode++)
+            {
+                // clear buffer for next mode
+                ssd1306_setbuf(0);
+
+                switch(mode)
+                {
+                    case 0:
+                        printf("buffer fill with binary\n\r");
+                        for(int i=0;i<sizeof(ssd1306_buffer);i++)
+                            ssd1306_buffer[i] = i;
+                        break;
+
+                    case 1:
+                        printf("pixel plots\n\r");
+                        for(int i=0;i<SSD1306_W;i++)
+                        {
+                            ssd1306_drawPixel(i, i/(SSD1306_W/SSD1306_H), 1);
+                            ssd1306_drawPixel(i, SSD1306_H-1-(i/(SSD1306_W/SSD1306_H)), 1);
+                        }
+                        break;
+
+                    case 2:
+                        {
+                            printf("Line plots\n\r");
+                            uint8_t y= 0;
+                            for(uint8_t x=0;x<SSD1306_W;x+=16)
+                            {
+                                ssd1306_drawLine(x, 0, SSD1306_W, y, 1);
+                                ssd1306_drawLine(SSD1306_W-x, SSD1306_H, 0, SSD1306_H-y, 1);
+                                y+= SSD1306_H/8;
+                            }
+                        }
+                        break;
+
+                    case 3:
+                        printf("Circles empty and filled\n\r");
+                        for(uint8_t x=0;x<SSD1306_W;x+=16)
+                            if(x<64)
+                                ssd1306_drawCircle(x, SSD1306_H/2, 15, 1);
+                            else
+                                ssd1306_fillCircle(x, SSD1306_H/2, 15, 1);
+                        break;
+                    case 4:
+                        printf("Image\n\r");
+                        ssd1306_drawImage(0, 0, bomb_i_stripped, 32, 32, 0);
+                        break;
+                    case 5:
+                        printf("Unscaled Text\n\r");
+                        ssd1306_drawstr(0,0, "This is a test", 1);
+                        ssd1306_drawstr(0,8, "of the emergency", 1);
+                        ssd1306_drawstr(0,16,"broadcasting", 1);
+                        ssd1306_drawstr(0,24,"system.",1);
+                        if(SSD1306_H>32)
+                        {
+                            ssd1306_drawstr(0,32, "Lorem ipsum", 1);
+                            ssd1306_drawstr(0,40, "dolor sit amet,", 1);
+                            ssd1306_drawstr(0,48,"consectetur", 1);
+                            ssd1306_drawstr(0,56,"adipiscing",1);
+                        }
+                        ssd1306_xorrect(SSD1306_W/2, 0, SSD1306_W/2, SSD1306_W);
+                        break;
+
+                    case 6:
+                        printf("Scaled Text 1, 2\n\r");
+                        ssd1306_drawstr_sz(0,0, "sz 8x8", 1, fontsize_8x8);
+                        ssd1306_drawstr_sz(0,16, "16x16", 1, fontsize_16x16);
+                        break;
+
+                    case 7:
+                        printf("Scaled Text 4\n\r");
+                        ssd1306_drawstr_sz(0,0, "32x32", 1, fontsize_32x32);
+                        break;
+
+
+                    case 8:
+                        printf("Scaled Text 8\n\r");
+                        ssd1306_drawstr_sz(0,0, "64", 1, fontsize_64x64);
+                        break;
+
+                    default:
+                        break;
+                }
+                ssd1306_refresh();
+
+                Delay_Ms(2000);
+            }
+        }
+    }
+    else
+        printf("failed.\n\r");
+
+    printf("Stuck here forever...\n\r");
     while(1);
 }
